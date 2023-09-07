d3fef81f902dac65f426e35429b6a67010972a7d
“Trijit <goswami.trijit@gmail.com>
2023-09-07 21:29:11 +0530

    commit

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,14 @@ async function bootstrap() {
       credentials: true 
   });
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(new ValidationPipe());
